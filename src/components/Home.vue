0feb6bccdde0f7178521124e74e82c0ae71be632
闫明港 <ymg618@126.com>
2020-08-03 12:56:34 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="info" @click="loginout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  methods: {
+    loginout() {
+      window.sessionStorage.clear()
+      this.$message('退出成功')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import './assets/fonts/iconfont.css'
 
+import './assets/css/global.css'
+import './plugins/element.js'
 Vue.config.productionTip = false
-
+// 配置请求根路径
+Vue.prototype.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 new Vue({
   router,
   render: h => h(App)
