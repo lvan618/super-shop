de884b327816b67ac8697e6bf3a235fd94b1ef6f
闫明港 <ymg618@126.com>
2020-08-06 10:41:28 +0800

    完成商品功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 import './assets/fonts/iconfont.css'
 
 import './assets/css/global.css'
@@ -17,6 +22,18 @@ axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '')
+  const d = (dt.getDate() + 1 + '').padStart(2, '')
+  const hh = (dt.getHours() + '').padStart(2, '')
+  const mm = (dt.getMinutes() + '').padStart(2, '')
+  const ss = (dt.getSeconds() + '').padStart(2, '')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+// 将富文本编辑器注册为全局可用
+Vue.use(VueQuillEditor)
 new Vue({
   router,
   render: h => h(App)
